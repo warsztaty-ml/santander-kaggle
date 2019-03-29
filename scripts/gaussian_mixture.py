a2334e8c23d0dbdd0a5ff6d6c2374576151f9dbc
Michał Kołodziej <kolodziejm355@gmail.com>
2019-03-29 17:57:03 +0100

    using functions from processing.py

--- scripts/gaussian_mixture.py
+++ scripts/gaussian_mixture.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 from sklearn import mixture
-
+import processing
 
 DATA_DIR = '../data/'
 
-component_number = 5
+component_number = 20
 
 clf = mixture.GaussianMixture(n_components=component_number, covariance_type='full', verbose=1)
 
@@ -15,42 +15,42 @@ def calculate_bic(data):
     return clf.bic(data)
 
 
-def calculate_score(x, y):
+def calculate_accuracy(x, y):
     y2 = np.array(clf.predict(x))
 
-    col_names = ['cluster_id', 'class', 'score', 'number of points']
+    col_names = ['cluster_id', 'class', 'accuracy', 'number of points']
     rows = []
 
     for i in range(component_number):
         cluster_indices = np.where(y2 == i)
         cluster = y[cluster_indices]
-        class_score = np.sum(cluster == 1)/len(cluster)
+        class_accuracy = np.sum(cluster == 1)/len(cluster)
         cluster_class = 1
-        if class_score < 0.5:
-            class_score = 1 - class_score
+        if class_accuracy < 0.5:
+            class_accuracy = 1 - class_accuracy
             cluster_class = 0
-        rows.append(np.array([i, cluster_class, class_score, len(cluster)]))
+        rows.append(np.array([i, cluster_class, class_accuracy, len(cluster)]))
 
     clusters = pd.DataFrame(data=rows, columns=col_names)
 
     print(clusters)
 
-    score = (clusters['score']*clusters['number of points']).sum()/clusters['number of points'].sum()
-    return score
+    accuracy = (clusters['accuracy']*clusters['number of points']).sum()/clusters['number of points'].sum()
+    return accuracy
 
 
 def gauss_mix_train():
     print("Loading train data")
     train = pd.read_csv(DATA_DIR + 'train.csv')
-    y_train = np.array(train[['target']]).flatten()
-    train.drop(['ID_code', 'target'], axis=1, inplace=True)
+
+    x_train, y_train, col_nr = processing.data_preprocessing(train, True)
 
     print("Training started")
-    clf.fit(train)
-    results = calculate_score(train, y_train)
-    print("Training finished. \nTraining score:\n{}".format(results))
+    clf.fit(x_train)
+    results = calculate_accuracy(x_train, y_train)
+    print("Training finished. \nTraining accuracy:\n{}".format(results))
 
-    bic = calculate_bic(train)
+    bic = calculate_bic(x_train)
     print("Bayesian information criterion: {}".format(bic))
 
 
@@ -63,8 +63,10 @@ def gauss_mix_test():
     print("Bayesian information criterion: {}".format(bic))
 
     print("Testing started")
-    results = clf.predict(test)
+    pred = clf.predict(test)
     print("Testing finished")
+
+    results = processing.data_postprocessing(test, pred)
 
     results.to_csv(DATA_DIR + 'gaussian_mixture_results.csv', index=False)
 
