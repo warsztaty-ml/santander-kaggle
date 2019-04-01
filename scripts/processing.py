88e31175e4cfae3391bfeb09091d98a17de1ec27
Michał Kołodziej <kolodziejm355@gmail.com>
2019-04-01 20:11:54 +0200

    processing: added data_preprocessing with oversample, added random seed to undersample

--- scripts/processing.py
+++ scripts/processing.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 from collections import Counter
 
 
@@ -18,15 +19,34 @@ def data_preprocessing(df, is_train):
     return df_out, y, len(df_out.columns)
 
 
-def data_preprocessing_with_undersampled(df, is_train):
+def data_preprocessing_with_undersampled(df, is_train, seed=0):
     target = 'target'
     y = None
     df_out = df.drop('ID_code', axis=1, inplace=False)
     scaler = StandardScaler()
     if is_train:
         print(df_out.shape)
-        rus = RandomUnderSampler(random_state=0)
+        rus = RandomUnderSampler(random_state=seed)
         X_resampled, y_resampled = rus.fit_resample(df_out, df_out[target])
+        print(X_resampled.shape, sorted(Counter(y_resampled).items()))
+        df_out = pd.DataFrame(X_resampled, columns=df_out.columns)
+        y = np.array(df_out[target], dtype=np.float64)
+        df_out.drop('target', axis=1, inplace=True)
+
+    df_out[df_out.columns] = scaler.fit_transform(df_out[df_out.columns])
+
+    return df_out, y, len(df_out.columns)
+
+
+def data_preprocessing_with_oversampled(df, is_train, seed=0):
+    target = 'target'
+    y = None
+    df_out = df.drop('ID_code', axis=1, inplace=False)
+    scaler = StandardScaler()
+    if is_train:
+        print(df_out.shape)
+        ros = RandomOverSampler(random_state=seed)
+        X_resampled, y_resampled = ros.fit_resample(df_out, df_out[target])
         print(X_resampled.shape, sorted(Counter(y_resampled).items()))
         df_out = pd.DataFrame(X_resampled, columns=df_out.columns)
         y = np.array(df_out[target], dtype=np.float64)
