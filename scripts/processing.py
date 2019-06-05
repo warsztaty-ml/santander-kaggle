de846e054081e621428e370ffe0371a1231e1670
Sebastian Sudra <Sebastian.Sudra@pentacomp.pl>
2019-06-05 22:03:56 +0200

    Uzupełenienie parametrów

--- scripts/processing.py
+++ scripts/processing.py
@@ -30,6 +30,25 @@ def data_preprocessing_with_undersampled(df, is_train, seed=0):
         X_resampled, y_resampled = rus.fit_resample(df_out, df_out[target])
         print("Liczba danych po zastosowaniu Undersampled:", X_resampled.shape[0], " w tym:", sorted(Counter(y_resampled).items()))
         df_out = pd.DataFrame(X_resampled, columns=df_out.columns)
+        y = np.array(df_out[target], dtype=np.float64)
+        df_out.drop('target', axis=1, inplace=True)
+
+    df_out[df_out.columns] = scaler.fit_transform(df_out[df_out.columns])
+
+    return df_out, y, len(df_out.columns)
+
+
+def data_preprocessing_with_undersampled2(df, is_train, seed=0):
+    target = 'target'
+    y = None
+    df_out = df.drop('ID_code', axis=1, inplace=False)
+    scaler = StandardScaler()
+    if is_train:
+        print("Liczba danych: ", df_out.shape[0], " w tym:", sorted(Counter(df_out[target]).items()))
+        rus = RandomUnderSampler(random_state=seed)
+        X_resampled, y_resampled = rus.fit_resample(df_out, df_out[target])
+        print("Liczba danych po zastosowaniu Undersampled:", X_resampled.shape[0], " w tym:", sorted(Counter(y_resampled).items()))
+        df_out = pd.DataFrame(X_resampled, columns=df_out.columns)
     y = np.array(df_out[target], dtype=np.float64)
     df_out.drop('target', axis=1, inplace=True)
 
--- scripts/svr.py
+++ scripts/svr.py
@@ -9,16 +9,24 @@ from sklearn import svm
 # Any results you write to the current directory are saved as output.
 INPUT_DIR = "../input/"
 
-def train_data_SVR(train, clf):
+def train_data_SVR(train, clf, version=1):
     print("Przeprowadzenie preprocessingu danych trenigowych")
-    x_train, y_train, input_size = pc.data_preprocessing_with_undersampled(train, True)
+    if version == 1:
+        x_train, y_train, input_size = pc.data_preprocessing_with_undersampled(train, True)
+    else:
+        x_train, y_train, input_size = pc.data_preprocessing_with_undersampled2(train, True)
+
     print("Rozpoczęcie procesu nauki")
     clf.fit(x_train, y_train)
 
 
-def test_data_SVR(test, clf):
+def test_data_SVR(test, clf, version=1):
     print("Przeprowadzenie preprocessingu danych testowych")
-    x_test_solo, _, _ = pc.data_preprocessing_with_undersampled(test, False)
+    if version == 1:
+        x_test_solo, _, _ = pc.data_preprocessing_with_undersampled(test, False)
+    else:
+        x_test_solo, _, _ = pc.data_preprocessing_with_undersampled2(test, False)
+
     print("Rozpoczęcie procesu testowania")
     pred_solo = clf.predict(x_test_solo)
     print("Przeprowadzenie postprocessingu danych testowych")
@@ -28,26 +36,6 @@ def test_data_SVR(test, clf):
 
 
 ###############################################################################
-
-def main():
-    print("Wczytywanie danych treningowych")
-    train = pd.read_csv(INPUT_DIR + 'train.csv')
-    print("Rozpoczęcie procesu nauki")
-    # train_data(train)
-    train_data_SVR(train)
-    del train
-    print("Rozpoczęcie procesu testowania")
-    test = pd.read_csv(INPUT_DIR + 'test.csv')
-    # submission = test_data(test)
-    submission = test_data_SVR(test)
-    print("Zapis odpowiedzi do pliku")
-    submission.to_csv('submission.csv', index=False)
-
-
-def roc_auc_score(y, pred):
-    return sklearn.metrics.roc_auc_score(y, pred)
-
-
 def args():
     """Parses command line arguments given to the script using argparse
     """
@@ -58,32 +46,56 @@ def args():
     parser.add_argument('--gamma', help='Gamma', default='auto', type=str)
     parser.add_argument('--c', help='C', default=1, type=float)
     parser.add_argument('--wsp', help='Data', default=1, type=float)
+    parser.add_argument('--train_data', help='Input train data csv', default='../data/train.csv', type=str)
+    parser.add_argument('--data', help='Input data csv', default='../data/test.csv', type=str)
+    parser.add_argument('--output', help='output path', default='svm_submission.csv', type=str)
 
     args = parser.parse_args()
     if args.gamma != 'auto':
         args.gamma = float(args.gamma)
 
     print(args)
-    return args.kernel, args.eps, args.gamma, args.c, args.wsp
+    return args.kernel, args.eps, args.gamma, args.c, args.wsp, args.train_data, args.data, args.output
+
+
+def main():
+    kernel, eps, gamma, c, wsp, train_data, data, output = args()
+    clf = svm.SVR(kernel=kernel, gamma=gamma, C=c, epsilon=eps)
+    print("Wczytywanie danych treningowych")
+    train = pd.read_csv(train_data)
+    num_rows = int(train.shape[0] * wsp)
+    train = train.head(num_rows)
+    print("Rozpoczęcie procesu nauki")
+    train_data_SVR(train, clf)
+    del train
+    print("Wczytywanie danych testowych")
+    test = pd.read_csv(data)
+    submission = test_data_SVR(test, clf)
+    print("Zapis odpowiedzi do pliku")
+    submission.to_csv(output, index=False)
+
+
+def roc_auc_score(y, pred):
+    return sklearn.metrics.roc_auc_score(y, pred)
 
 
 def main2():
-    kernel, eps, gamma, c, wsp = args()
+    kernel, eps, gamma, c, wsp, train_data, data, output = args()
     clf = svm.SVR(kernel=kernel, gamma=gamma, C=c, epsilon=eps)
     print("Wczytywanie danych")
-    data = pd.read_csv(INPUT_DIR + 'train.csv')
+    data = pd.read_csv(train_data)
     num_rows = data.shape[0]
     num_rows = int(num_rows * wsp)
     k = int(num_rows * 9 / 10)
     train = data.head(k)
     test = data.tail(num_rows - k)
-    train_data_SVR(train, clf)
+    train_data_SVR(train, clf, 2)
 
     y_test = test[["target"]]
-    submission = test_data_SVR(test, clf)
+    submission = test_data_SVR(test, clf, 2)
     pred = submission[["target"]]
     print("Otrzymany rezultat:", roc_auc_score(np.array(y_test, dtype=np.float64), pred))
 
 
 if __name__ == '__main__':
-    main2()
+    main()
